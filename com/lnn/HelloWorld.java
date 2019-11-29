c70fb03d7d2502683a3c4001797dfbc78d69c7fd
Nambi Narayanan <nambilakshmanan@gmail.com>
2019-11-29 21:37:01 +0100

    updated hello world to hello aarthy

--- com/lnn/HelloWorld.java
+++ com/lnn/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.lnn;
 public class HelloWorld{
   public static void main(String[] args){
-    System.out.println("Hello World...Hi Jenkins...");
+    System.out.println("Hello Aarthy...Hi Jenkins...");
   }
 }
